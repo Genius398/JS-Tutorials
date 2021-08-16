3ee19e7b80f3fd44dd0a47f8f204a8e0a78abf7b
Anantjit <anantjitchander@gmail.com>
2021-08-16 17:01:15 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("body").onscroll = function myFunction() {
-  var scrolltotop = document.scrollingElement.scrollTop;
-  var target = document.getElementById("body");
-  var xvalue = "center";
-  var factor = 0.5;
-  var yvalue = scrolltotop * factor;
-  target.style.backgroundPosition = xvalue + " " + yvalue + "px";
-};
